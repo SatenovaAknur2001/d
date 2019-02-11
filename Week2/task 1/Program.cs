1f39062c2b1e6383f6af5aa3e6f9ae4668e3f568
SatenovaAknur2001 <aknur.satenova.01@inbox.ru>
2019-02-11 21:45:38 +0600

    second lab

---
+++ Week2/task 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"C:\Users\User\Desktop\pp2\task 1.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sw = new StreamReader(fs))
+                {
+                    string text = sw.ReadToEnd();
+                    Console.WriteLine(text);
+
+                    bool pol = true;
+                    for (int i = 0; i < text.Length; i++)     // проверяю первую и последнюю, вторую и предпосл, и тд
+                    {
+                        if (text[i] == text[text.Length - 1 - i])
+                        {
+                            pol = true;
+                        }
+                        else
+                        {
+                            pol = false;
+                            break;
+                        }
+                    }
+                    if (pol) Console.WriteLine("Yes");      //вывод ответа
+                    else Console.WriteLine("No");
+                }
+            }
+        }
+    }
+}
---
+++ Week2/task 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TASK_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(@"C:\Users\User\Desktop\pp2\path\newf.txt", FileMode.Create)) { } //создал файл, если сущ то перезаписывается
+
+            string fileName = "newf.txt";  //сам файл который долж скопироаваться
+            string path = @"C:\Users\User\Desktop\pp2\path";      //источник
+            string path1 = @"C:\Users\User\Desktop\pp2\path1";    //цель
+
+            string Ffile = Path.Combine(path, fileName);  // сведения о пути
+            string Sfile = Path.Combine(path1, fileName);
+
+
+            File.Copy(Ffile, Sfile, true);   //копирование
+            File.Delete(@"C:\Users\User\Desktop\pp2\path\newf.txt"); // удаление оригинала
+
+        }
+    }
+}
